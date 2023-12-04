19d1b0ad8b283ccea66547623733beacd7cb92d2
Grisha <sibrim1212@gmail.com>
2023-12-05 00:47:49 +0400

    "first increment fix"

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -57,7 +57,7 @@ func mainHandler(res http.ResponseWriter, req *http.Request) {
 		if !ok {
 			res.WriteHeader(http.StatusBadRequest)
 		} else {
-			res.Header().Add("Location", originalURL)
+			res.Header().Add("location", originalURL)
 			res.WriteHeader(http.StatusTemporaryRedirect)
 		}
 	} else {
