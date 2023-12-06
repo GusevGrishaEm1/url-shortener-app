0bed0d26f6765999b9d6537849cc6eb680679fc1
Grisha <sibrim1212@gmail.com>
2023-12-07 01:00:07 +0400

    "second increment completed"

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -9,10 +9,9 @@ import (
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-var urls map[string]string
+var urls map[string]string = make(map[string]string)
 
 func Init() {
-	urls = make(map[string]string)
 	mux := initHandlers()
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
@@ -22,35 +21,23 @@ func Init() {
 
 func initHandlers() *http.ServeMux {
 	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", mainHandler)
-
+	mux.HandleFunc("/", ShortenerHandler)
 	return mux
 }
 
-func getShortURL() string {
-	shortURL := make([]byte, 5)
-	var uniqueShortURL string
-	for {
-		for i := range shortURL {
-			shortURL[i] = letterBytes[rand.Intn(len(letterBytes))]
-		}
-		uniqueShortURL = string(shortURL)
-		if _, ok := urls[uniqueShortURL]; !ok {
-			return uniqueShortURL
-		}
-	}
-}
-
-func mainHandler(res http.ResponseWriter, req *http.Request) {
+func ShortenerHandler(res http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodPost {
-		body, _ := io.ReadAll(req.Body)
-		bodyStr := string(body)
-		shortURL := getShortURL()
-		urls[shortURL] = bodyStr
-		res.Header().Add("content-type", "text/plain")
-		res.WriteHeader(http.StatusCreated)
-		res.Write([]byte("http://localhost:8080/" + shortURL))
+		body, err := io.ReadAll(req.Body)
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+		} else {
+			bodyStr := string(body)
+			shortURL := GetShortURL()
+			urls[shortURL] = bodyStr
+			res.Header().Add("content-type", "text/plain")
+			res.WriteHeader(http.StatusCreated)
+			res.Write([]byte("http://localhost:8080/" + shortURL))
+		}
 	} else if req.Method == http.MethodGet {
 		urlsParts := strings.Split(req.URL.Path, "/")
 		shortURL := urlsParts[len(urlsParts)-1]
@@ -63,5 +50,19 @@ func mainHandler(res http.ResponseWriter, req *http.Request) {
 		}
 	} else {
 		res.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+func GetShortURL() string {
+	shortURL := make([]byte, 5)
+	var uniqueShortURL string
+	for {
+		for i := range shortURL {
+			shortURL[i] = letterBytes[rand.Intn(len(letterBytes))]
+		}
+		uniqueShortURL = string(shortURL)
+		if _, ok := urls[uniqueShortURL]; !ok {
+			return uniqueShortURL
+		}
 	}
 }
---
+++ internal/app/server/server_test.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestShortenerHandler(t *testing.T) {
+	var host string = "http://localhost:8080/"
+	var originalURL string = "https://practicum.yandex.ru/"
+	var shortURL string
+	t.Run("test POST", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodPost, "/", bytes.NewReader([]byte(originalURL)))
+		w := httptest.NewRecorder()
+		ShortenerHandler(w, request)
+		res := w.Result()
+		assert.Equal(t, 201, res.StatusCode)
+		defer res.Body.Close()
+		resBody, err := io.ReadAll(res.Body)
+		require.NoError(t, err)
+		partsURL := strings.Split(string(resBody), "/")
+		shortURL = partsURL[len(partsURL)-1]
+		assert.Equal(t, string(resBody), host+shortURL)
+	})
+	t.Run("test GET", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodGet, "/"+shortURL, nil)
+		w := httptest.NewRecorder()
+		ShortenerHandler(w, request)
+		res := w.Result()
+		assert.Equal(t, 307, res.StatusCode)
+		assert.Equal(t, res.Header.Get("Location"), originalURL)
+	})
+}
