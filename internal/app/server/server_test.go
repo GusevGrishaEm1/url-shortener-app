3c8bdf6d65e8a2721c46893a41d20c25b49ff687
Grisha <sibrim1212@gmail.com>
2023-12-07 01:12:50 +0400

    "second increment fix"

--- internal/app/server/server_test.go
+++ internal/app/server/server_test.go
@@ -34,8 +34,8 @@ func TestShortenerHandler(t *testing.T) {
 		w := httptest.NewRecorder()
 		ShortenerHandler(w, request)
 		res := w.Result()
-		defer res.Body.Close()
 		assert.Equal(t, 307, res.StatusCode)
 		assert.Equal(t, res.Header.Get("Location"), originalURL)
+		defer res.Body.Close()
 	})
 }
