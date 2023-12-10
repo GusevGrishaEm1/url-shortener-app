e32652ebb06828dc25c2f45f302c16b733d37eed
Grisha <sibrim1212@gmail.com>
2023-12-10 16:55:18 +0400

    "fifth increment fix"

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/GusevGrishaEm1/url-shortener-app.git/internal/app/server"
 )
 
 func main() {
-	envList := os.Environ()
-	// выводим первые пять элементов
-	for i := 0; i < len(envList); i++ {
-		fmt.Println(envList[i])
-	}
 	config := parseFlags()
 	server.Init(config)
 }
