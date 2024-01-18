f9ab6429f1d0a0263ad2423f7ca14b29b7b105d4
Grisha <sibrim1212@gmail.com>
2024-01-19 00:44:44 +0400

    tenth increment fix

--- internal/app/storage/storage.go
+++ internal/app/storage/storage.go
@@ -13,7 +13,7 @@ type URLStorage interface {
 }
 
 func New(fileStoragePath string) (URLStorage, error) {
-	file, err := os.OpenFile(fileStoragePath, os.O_RDONLY, 0666)
+	file, err := os.OpenFile(fileStoragePath, os.O_RDONLY|os.O_CREATE, 0666)
 	if err != nil {
 		return nil, err
 	}
