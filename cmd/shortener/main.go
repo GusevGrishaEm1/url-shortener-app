d72c1a26d15a1064f1f519c5f502dc6db6100663
Grisha <sibrim1212@gmail.com>
2024-02-06 23:35:04 +0400

    iter13 fix

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -8,14 +8,10 @@ import (
 )
 
 func main() {
-	var err error
-	err = logger.Init(slog.LevelInfo)
-	if err != nil {
+	if err := logger.Init(slog.LevelInfo); err != nil {
 		panic(err)
 	}
-	config := parseFlagsAndEnv()
-	err = server.StartServer(config)
-	if err != nil {
+	if err := server.StartServer(parseFlagsAndEnv()); err != nil {
 		panic(err)
 	}
 }
