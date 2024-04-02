6b6e24d3017dc64d273c941b6e1fadf0087f07bf
GusevGrishaEm1 <sibrim1212@gmail.com>
2024-04-02 22:12:08 +0300

    remove trash code

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"context"
-	"os"
-	"runtime/pprof"
-	"time"
 
 	"github.com/GusevGrishaEm1/url-shortener-app.git/internal/app/config"
 	"github.com/GusevGrishaEm1/url-shortener-app.git/internal/app/server"
@@ -13,21 +10,7 @@ import (
 func main() {
 	ctx := context.Background()
 	config := config.Init()
-	go pprofSaveToFileHeapInfo()
 	if err := server.StartServer(ctx, config); err != nil {
 		panic(err)
 	}
-}
-
-func pprofSaveToFileHeapInfo() {
-	time.Sleep(30 * time.Second)
-	f, err := os.Create("profiles/base2.pprof")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	if err := pprof.WriteHeapProfile(f); err != nil {
-		panic(err)
-	}
-	println("Memory profile saved to profiles/base2.pprof")
 }
