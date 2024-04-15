5d54da4c7ba31bec7ec05c44a05ae82cab66fd57
GusevGrishaEm1 <sibrim1212@gmail.com>
2024-04-15 22:34:51 +0400

    iter20

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/GusevGrishaEm1/url-shortener-app.git/internal/app/config"
 	"github.com/GusevGrishaEm1/url-shortener-app.git/internal/app/server"
 )
 
+var (
+	buildVersion string = "N/A"
+	buildDate    string = "N/A"
+	buildCommit  string = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s\n", buildVersion, buildDate, buildCommit)
 	ctx := context.Background()
 	config := config.New()
 	if err := server.StartServer(ctx, config); err != nil {
