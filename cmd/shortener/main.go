7c21e7bfac6f95934d7b55568e68f156a02427ba
GusevGrishaEm1 <sibrim1212@gmail.com>
2024-05-30 00:31:00 +0400

    fix

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -17,16 +17,23 @@ var (
 )
 
 func main() {
+	if err := runHTTP(); err != nil {
+		panic(err)
+	}
+}
+
+func runHTTP() error {
 	fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s\n", buildVersion, buildDate, buildCommit)
 	ctx := context.Background()
 	config, err := config.New()
 	if err != nil {
-		panic(err)
+		return err
 	}
 	if err := http_server.StartServer(ctx, config); err != nil {
 		if errors.Is(err, http.ErrServerClosed) {
-			return
+			return nil
 		}
-		panic(err)
+		return err
 	}
+	return nil
 }
